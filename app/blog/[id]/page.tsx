b62fe1de5732c4a64e086f5ed60a87700d4991ea
Diego Oliveira <di.oliveira2004@gmail.com>
2024-12-14 10:21:26 -0800

    Update page.tsx

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -24,6 +24,27 @@ export async function generateMetadata({ params }: BlogPostPageProps): Promise<M
   return {
     title: `${post.title} - CCTV Installation Blog`,
     description: post.excerpt,
+    openGraph: {
+      title: post.title,
+      description: post.excerpt,
+      type: 'article',
+      publishedTime: post.date,
+      authors: [post.author],
+      images: [
+        {
+          url: post.image,
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: post.title,
+      description: post.excerpt,
+      images: [post.image],
+    },
   }
 }
 
